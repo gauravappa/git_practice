bc39093a8b3b07eaed8c112d6226fe73ec6f4cf7
gauravappa <gauravappagga@gmail.com>
2023-02-13 08:30:51 +0530

    edited complete

--- src/main/java/com/sample/gitpractice/Student.java
+++ src/main/java/com/sample/gitpractice/Student.java
@@ -2,6 +2,18 @@ package com.sample.gitpractice;
 
 public class Student {
 
-  String name;
-  
+
+	  String name;
+	
+	String fullName;
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	
 }
